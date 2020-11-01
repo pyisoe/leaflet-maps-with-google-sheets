3b90840347316148a47255372b7c789c8935b68b
pyisoe <pyisoester@gmail.com>
2020-11-02 01:06:25 +0630

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GNlTuRDm8RQ_LtRaqvxBMBi_YUYvCBEkItlK-qfSyeo/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyB0kS8WPiO8gQFfA2T5FV45sl77a7nkAsI';
